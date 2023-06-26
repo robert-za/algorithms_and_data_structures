016c696d767c41f968eb7e6f006cc20fba955576
robert-za <robert.zaniewski@gadz.org>
2023-06-26 22:37:35 +0200

    Section 6 completed

--- 02_02_begin/bfs.py
+++ 02_02_begin/bfs.py
@@ -9,8 +9,22 @@ from helpers import get_path, offsets, is_legal_pos, read_maze
 from queue_ll import Queue
 
 
-def bfs(maze, start, goal):
-    pass
+def bfs(maze, start, goal) -> list | None:
+    queue = Queue()
+    queue.enqueue(start)
+    pred = {start: None}
+
+    while not queue.is_empty():
+        current_cell = queue.dequeue()
+        if current_cell == goal:
+            return get_path(pred, start, goal)
+        for direction in ["up", "right", "down", "left"]:
+            row_offset, col_offset = offsets[direction]
+            neighbour = (current_cell[0] + row_offset, current_cell[1] + col_offset)
+            if is_legal_pos(maze, neighbour) and neighbour not in pred:
+                queue.enqueue(neighbour)
+                pred[neighbour] = current_cell
+    return None
 
 
 if __name__ == "__main__":
@@ -22,7 +36,7 @@ if __name__ == "__main__":
     assert result == [(0, 0), (0, 1), (0, 2), (1, 2), (2, 2)]
 
     # Test 2
-    maze = read_maze("mazes/mini_maze_bfs.txt")
+    maze = read_maze("mini_maze_bfs.txt")
     # for row in maze:
     #     print(row)
     start_pos = (0, 0)
@@ -31,7 +45,7 @@ if __name__ == "__main__":
     assert result == [(0, 0), (1, 0), (1, 1), (1, 2), (2, 2)]
 
     # Test 3
-    maze = read_maze("mazes/mini_maze_bfs.txt")
+    maze = read_maze("mini_maze_bfs.txt")
     start_pos = (0, 0)
     goal_pos = (3, 3)
     result = bfs(maze, start_pos, goal_pos)
