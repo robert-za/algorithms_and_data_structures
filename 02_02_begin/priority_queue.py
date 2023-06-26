dd310324fc21a36429aa64080dc41c91cb985a4d
robert-za <robert.zaniewski@gadz.org>
2023-06-26 23:19:54 +0200

    Section 7 completed

--- 02_02_begin/priority_queue.py
+++ 02_02_begin/priority_queue.py
@@ -8,4 +8,38 @@ import heapq
 
 
 class PriorityQueue:
-    pass
+    def __init__(self) -> None:
+        self.elements = []
+
+    def is_empty(self) -> bool:
+        return not self.elements
+
+    def put(self, item, priority) -> None:
+        heapq.heappush(self.elements, (priority, item))
+
+    def get(self):
+        return heapq.heappop(self.elements)[1]
+
+    def __str__(self) -> str:
+        return str(self.elements)
+
+
+if __name__ == "__main__":
+    pq = PriorityQueue()
+    print(pq)
+    print(pq.is_empty())
+
+    # item, priority
+    pq.put("eat", 2)
+    pq.put("coding", 1)
+    pq.put("sleep", 3)
+    pq.put("friends", 3)
+
+
+    print(pq)
+
+    print(pq.get())
+    print(pq.get())
+    print(pq.get())
+
+    print(pq)
